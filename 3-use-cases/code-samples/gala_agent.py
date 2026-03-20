89e8eed71fe9aac686ffd6620da563667bf7e14c
Đặng Minh Ngôn <dangminhngon.work@gmail.com>
2026-03-20 15:16:40 +0700

    Unit 3: UseCase for Agentic RAG

---
+++ 3-use-cases/code-samples/gala_agent.py
@@ -0,0 +1,73 @@
+from typing import TypedDict, Annotated
+from langgraph.graph.message import add_messages
+from langchain_core.messages import AnyMessage, HumanMessage, AIMessage
+from langgraph.prebuilt import ToolNode
+from langgraph.graph import START, StateGraph
+from langgraph.prebuilt import tools_condition
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+
+from tools import DuckDuckGoSearchRun, weather_info_tool, hub_stats_tool
+from retriever import guest_info_tool
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
+
+
+# Initialize the web search tool
+search_tool = DuckDuckGoSearchRun()
+
+# Generate the chat interface, including the tools
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-Coder-32B-Instruct",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+)
+
+
+chat = ChatHuggingFace(llm=llm, verbose=True)
+tools = [guest_info_tool, search_tool, weather_info_tool, hub_stats_tool]
+chat_with_tools = chat.bind_tools(tools)
+
+
+# Generate the AgentState and Agent graph
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
+
+
+def assistant(state: AgentState):
+    return {
+        "messages": [chat_with_tools.invoke(state["messages"])],
+    }
+
+
+## The graph
+builder = StateGraph(AgentState)
+
+builder.add_node("assistant", assistant)
+builder.add_node("tools", ToolNode(tools))
+
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges(
+    "assistant",
+    # If the latest message requires a tool, route to tools
+    # Otherwise, provide a direct response
+    tools_condition,
+)
+builder.add_edge("tools", "assistant")
+alfred = builder.compile()
+
+# Example 1:
+# response = alfred.invoke({"messages": "Tell me about 'Lady Ada Lovelace'"})
+#
+# print("🎩 Alfred's Response:")
+# print(response["messages"][-1].content)
+
+# Example 2:
+response = alfred.invoke(
+    {
+        "messages": "One of our guests is from Qwen. What can you tell me about their most popular model?"
+    }
+)
+
+print("🎩 Alfred's Response:")
+print(response["messages"][-1].content)
