4aafae9fef0bf384882444b32bd2236a0be5ea49
Đặng Minh Ngôn <dangminhngon.work@gmail.com>
2026-03-21 16:29:06 +0700

    Unit 4: Final AI Agent Project

---
+++ 4-final-project/code-samples/agent.py
@@ -0,0 +1,117 @@
+import os
+from langgraph.graph import START, END, StateGraph, MessagesState
+from langgraph.prebuilt import tools_condition
+from langgraph.prebuilt import ToolNode
+from dotenv import load_dotenv, find_dotenv
+from langchain_core.tools import tool
+from langchain_tavily import TavilySearch
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_litellm import ChatLiteLLM
+from langchain_groq import ChatGroq
+
+load_dotenv(find_dotenv())
+
+
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers.
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+
+@tool
+def add(a: int, b: int) -> int:
+    """Add two numbers.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a + b
+
+
+@tool
+def subtract(a: int, b: int) -> int:
+    """Subtract two numbers.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a - b
+
+
+@tool
+def divide(a: int, b: int) -> int:
+    """Divide two numbers.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
+
+
+@tool
+def modulus(a: int, b: int) -> int:
+    """Get the modulus of two numbers.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a % b
+
+
+@tool
+def web_search(query: str) -> str:
+    """Search Tavily for a query and return maximum 3 results.
+
+    Args:
+        query: The search query."""
+    search_docs = TavilySearch(max_results=3).invoke(query=query)
+    formatted_search_docs = "\n\n---\n\n".join(
+        [
+            f'<Document source="{doc.metadata["source"]}" page="{doc.metadata.get("page", "")}"/>\n{doc.page_content}\n</Document>'
+            for doc in search_docs
+        ]
+    )
+    return {"web_results": formatted_search_docs}
+
+
+tools = [add, divide, multiply, subtract, modulus, web_search]
+
+
+def build_agent():
+    llm = ChatGroq(model="qwen/qwen3-32b", temperature=0)
+
+    # model_name = "ollama_chat/qwen2.5-coder:14b"
+    # llm = ChatLiteLLM(
+    #     model=model_name,
+    #     api_base="http://localhost:11434",
+    #     temperature=0,  # Keep at 0 for reliable tool calling
+    # )
+
+    llm_with_tools = llm.bind_tools(tools)
+
+    # Node
+    def assistant(state: MessagesState):
+        return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+    builder = StateGraph(MessagesState)
+    builder.add_node("assistant", assistant)
+    builder.add_node("tools", ToolNode(tools))
+    builder.add_edge(START, "assistant")
+    builder.add_conditional_edges("assistant", tools_condition)
+    builder.add_edge("tools", "assistant")
+
+    return builder.compile()
+
+
+if __name__ == "__main__":
+    build_agent()
